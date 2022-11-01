66963760f5e91cd555dbc8f514c6fba815b1f3f1
pravallikaadineni <pravallikaadineni143@gmail.com>
2022-11-01 19:31:53 +0530

    upload

---
+++ AzureProject/webformsproject/sclass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+
+namespace webformsproject
+{
+    public class sclass
+    {
+        DataSet ds = null;
+        SqlDataAdapter da = null;
+        SqlConnection sql = null;
+        public sclass()
+        {
+            sql = new SqlConnection("Data Source = projectsql.database.windows.net; Initial Catalog = rainbowdb; Persist Security Info=True; User ID = avs; Password = ***********;Integrated Security=True");
+            ds = new DataSet();
+        }
+        private DataTable Connect()
+        {
+            da = new SqlDataAdapter("select * from class", sql);
+
+
+
+            da.Fill(ds, "class");
+            DataTable dt_proddata = ds.Tables["class"];
+            return dt_proddata;
+        }
+
+        public string Findclass(int classno)
+        {
+            DataTable class3 = Connect();
+            DataColumn[] keycolumns = new DataColumn[1];
+            keycolumns[0] = class3.Columns["classno"];
+            class3.PrimaryKey = keycolumns;
+            DataRow dr = class3.Rows.Find(classno);
+            string ans = dr[0].ToString() + " " + dr[1].ToString();
+            return ans;
+        }
+    }
+}
